0a5853f5c676f8f1b099b8a32273ddbbd9423c19
João Martins <martins.joao1909@gmail.com>
2023-03-27 19:17:26 -0300

    chore: field structure updated

--- smart-cars/Car.js
+++ smart-cars/Car.js
@@ -10,6 +10,10 @@ class Car {
     right: null,
   };
 
+  GRID_TILE = 11;
+  EVEN_TILE_CORRECTION = 5;
+  FONT_CORRECTION = 1.4;
+
   constructor(fieldPosition) {
     this.fieldPosition = fieldPosition;
     this.setupCar();
@@ -17,19 +21,14 @@ class Car {
   }
 
   updateFildSize(fieldPosition) {
-    this.position = {
-      x: this.fieldPosition.x + 1,
-      y: this.fieldPosition.y + 1,
-    };
     this.fieldPosition = fieldPosition;
+    this.resetCarPosition();
   }
 
   setupCar() {
-    this.position = {
-      x: this.fieldPosition.x + 1,
-      y: this.fieldPosition.y + 1,
-    };
+    this.resetCarPosition();
     this.car = document.createElement("span");
+    this.car.setAttribute("class", "car");
   }
 
   setControllers(controllers) {
@@ -57,51 +56,57 @@ class Car {
     switch (direction) {
       case "up":
       case "ArrowUp":
-        if (this.position.y <= this.fieldPosition.y + 1) return;
-        this.position.y -= 11;
+        if (this.position.y <= this.fieldPosition.y + this.GRID_TILE + 1)
+          return;
+        this.position.y -= this.GRID_TILE;
         break;
 
       case "right":
       case "ArrowRight":
         if (
           this.position.x >=
-          this.fieldPosition.length.x + this.fieldPosition.x - 11
+          this.fieldPosition.length.x + this.fieldPosition.x - this.GRID_TILE
         )
           return;
-        this.position.x += 11;
+        this.position.x += this.GRID_TILE;
         break;
 
       case "down":
       case "ArrowDown":
         if (
           this.position.y >=
-          this.fieldPosition.length.y + this.fieldPosition.y - 11
+          this.fieldPosition.length.y + this.fieldPosition.y - this.GRID_TILE
         )
           return;
-        this.position.y += 11;
+        this.position.y += this.GRID_TILE;
         break;
 
       case "left":
       case "ArrowLeft":
         if (this.position.x <= this.fieldPosition.x + 1) return;
-        this.position.x -= 11;
+        this.position.x -= this.GRID_TILE;
         break;
     }
+  }
+
+  resetCarPosition() {
+    let y =
+      this.fieldPosition.y +
+      Math.floor(this.fieldPosition.length.y / 2) +
+      this.FONT_CORRECTION -
+      this.GRID_TILE;
+
+    if (this.fieldPosition.length.y % 2 === 0) y += this.EVEN_TILE_CORRECTION;
+
+    this.position = { x: 49 + 1, y };
   }
 
   draw() {
     this.car.setAttribute(
       "style",
       `
-        display: flex;
-        background-color: blue;
-        height: 10px;
-        width: 10px;
-        position: absolute;
         top: ${this.position.y}px;
         left: ${this.position.x}px;
-        border-radius: 0 100px 100px 0;
-        box-shadow: white 0px 0px 11px 1px;
       `
     );
   }
