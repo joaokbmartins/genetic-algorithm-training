833b60cf93b75093e60e066d14fdb006f009d99e
João Martins <martins.joao1909@gmail.com>
2023-03-28 20:59:29 -0300

    chore: target class added

---
+++ smart-cars/Target.js
@@ -0,0 +1,28 @@
+class Target {
+  element = null;
+  position = { x: 0, y: 0 };
+
+  constructor() {
+    this.createElement();
+  }
+
+  createElement() {
+    this.element = document.createElement("span");
+    this.element.setAttribute("class", "target");
+  }
+
+  updatePosition(position) {
+    this.position = position;
+    this.draw();
+  }
+
+  draw() {
+    this.element.setAttribute(
+      "style",
+      `
+        top: ${this.position.y}px;
+        left: ${this.position.x}px;
+      `
+    );
+  }
+}
